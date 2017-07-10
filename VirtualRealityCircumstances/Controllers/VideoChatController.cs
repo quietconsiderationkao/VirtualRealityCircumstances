d4b48e8ff07e6c4fcf262f187df8d15bc23f70e9
高静思 <gaojingsi_love@hotmail.com>
2017-07-10 09:24:49 +0200

    VideoChat section need to adjust.

---
+++ VirtualRealityCircumstances/Controllers/VideoChatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VirtualRealityCircumstances.Controllers
+{
+    public class VideoChatController : BaseController
+    {
+        // GET: VideoChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Room()
+        {
+            return View();
+        }
+    }
+}
---
+++ VirtualRealityCircumstances/Scripts/VideoChat/Chat.js
@@ -0,0 +1,37 @@
+﻿window.onload = function (e) {
+    var video = document.getElementById('player');
+    var range = document.querySelector('input');
+
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(function (stream) {
+        /* use the stream */
+        video.srcObject = stream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+            video.muted = true;
+        };
+        // Create a MediaStreamAudioSourceNode
+        // Feed the HTMLMediaElement into it
+        var audioCtx = new AudioContext();
+        audioCtx.sampleRate = 22050;
+
+        var source = audioCtx.createMediaStreamSource(stream);
+        // Create a biquadfilter
+        var biquadFilter = audioCtx.createBiquadFilter();
+        biquadFilter.type = "lowshelf";
+        biquadFilter.frequency.value = 1000;
+        biquadFilter.gain.value = range.value;
+        // connect the AudioBufferSourceNode to the gainNode
+        // and the gainNode to the destination, so we can play the
+        // music and adjust the volume using the mouse cursor
+        source.connect(biquadFilter);
+        biquadFilter.connect(audioCtx.destination);
+        // Get new mouse pointer coordinates when mouse is moved
+        // then set new gain value
+        range.oninput = function () {
+            biquadFilter.gain.value = range.value;
+        }
+
+    }).catch(function (err) {
+        /* handle the error */
+    });
+}
